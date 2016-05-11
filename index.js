a48594393313216d2681256c6a84fd5a5ce9787d
Isaiah Turla <turlaisaiah@hotmail.com>
2016-05-11 23:07:34 +1000

    removed x-powered-by header

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(main.handleInvalidJson);
+app.disable('x-powered-by');
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
